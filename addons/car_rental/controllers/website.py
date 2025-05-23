48e8ac50e9b7699c961c27fe348fc73c50266718
11kamal11 <kamal9@emails.emizentech.com>
2025-05-23 16:31:09 +0530

    Update website.py

--- addons/car_rental/controllers/website.py
+++ addons/car_rental/controllers/website.py
@@ -7,8 +7,15 @@ class CarRentalWebsite(http.Controller):
         cars = http.request.env['car.rental.car'].search([('is_available', '=', True)])
         return http.request.render('car_rental.car_listing', {'cars': cars})
 
-   
-@http.route('/available-for-rent', type='http', auth='public', website=True)
-def available_for_rent(self, **kw):
-    cars = http.request.env['car.rental.car'].search([('is_available', '=', True)])
-    return http.request.render('car_rental.available_for_rent', {'cars': cars})
+    @http.route('/available-for-rent', type='http', auth='public', website=True)
+    def available_for_rent(self, category=None, **kw):
+        domain = [('is_available', '=', True)]
+        if category:
+            domain.append(('category_id.name', '=', category))
+        cars = http.request.env['car.rental.car'].search(domain)
+        categories = http.request.env['car.rental.category'].search([])
+        return http.request.render('car_rental.car_listing', {
+            'cars': cars,
+            'categories': categories,
+            'category': category
+        })
