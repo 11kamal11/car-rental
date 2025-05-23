ef2e6f8be3796dd2eee2541e6b62f1209a080143
11kamal11 <kamal9@emails.emizentech.com>
2025-05-23 16:42:07 +0530

    Update website.py

--- addons/car_rental/controllers/website.py
+++ addons/car_rental/controllers/website.py
@@ -8,14 +8,36 @@ class CarRentalWebsite(http.Controller):
         return http.request.render('car_rental.car_listing', {'cars': cars})
 
     @http.route('/available-for-rent', type='http', auth='public', website=True)
-    def available_for_rent(self, category=None, **kw):
+    def available_for_rent(self, category=None, search=None, **kw):
         domain = [('is_available', '=', True)]
         if category:
             domain.append(('category_id.name', '=', category))
+        if search:
+            domain.append('|', ('name', 'ilike', search), ('model', 'ilike', search))
         cars = http.request.env['car.rental.car'].search(domain)
         categories = http.request.env['car.rental.category'].search([])
         return http.request.render('car_rental.car_listing', {
             'cars': cars,
             'categories': categories,
-            'category': category
+            'category': category,
+            'search': search
         })
+
+    @http.route('/book-car/<int:car_id>', type='http', auth='public', website=True)
+    def book_car(self, car_id, **kw):
+        car = http.request.env['car.rental.car'].sudo().browse(car_id)
+        if not car.exists():
+            return http.request.redirect('/available-for-rent')
+        return http.request.render('car_rental.car_booking_form', {'car': car})
+
+    @http.route('/submit-booking', type='http', auth='public', website=True)
+    def submit_booking(self, **kw):
+        car_id = int(kw.get('car_id'))
+        name = kw.get('name')
+        rental_date = kw.get('rental_date')
+        http.request.env['car.rental.booking'].sudo().create({
+            'car_id': car_id,
+            'name': name,
+            'rental_date': rental_date,
+        })
+        return http.request.redirect('/available-for-rent?booking=success')
