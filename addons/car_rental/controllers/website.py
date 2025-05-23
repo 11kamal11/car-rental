0421627cb029f5f9732e31af4b537270495631ec
11kamal11 <kamal9@emails.emizentech.com>
2025-05-23 15:30:11 +0530

    Update website.py

--- addons/car_rental/controllers/website.py
+++ addons/car_rental/controllers/website.py
@@ -6,3 +6,8 @@ class CarRentalWebsite(http.Controller):
     def car_list(self, **kw):
         cars = http.request.env['car.rental.car'].search([('is_available', '=', True)])
         return http.request.render('car_rental.car_listing', {'cars': cars})
+
+    @http.route('/available-for-rent', type='http', auth='public', website=True)
+    def available_for_rent(self, **kw):
+        cars = http.request.env['car.rental.car'].search([('is_available', '=', True)])
+        return http.request.render('car_rental.car_listing', {'cars': cars})
