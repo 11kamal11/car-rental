3868404267c7fa62d4231bc3f21aa4c413133232
11kamal11 <kamal9@emails.emizentech.com>
2025-05-23 16:38:59 +0530

    Update car.py

--- addons/car_rental/models/car.py
+++ addons/car_rental/models/car.py
@@ -11,9 +11,18 @@ class CarRentalCar(models.Model):
     image_1920 = fields.Image(string='Image')
     category_id = fields.Many2one('car.rental.category', string='Category')
     description = fields.Text(string='Description')
+    license_plate = fields.Char(string='License Plate')  # Added field
 
 class CarRentalCategory(models.Model):
     _name = 'car.rental.category'
     _description = 'Car Rental Category'
 
     name = fields.Char(string='Category Name', required=True)
+
+class CarRentalBooking(models.Model):
+    _name = 'car.rental.booking'
+    _description = 'Car Rental Booking'
+
+    car_id = fields.Many2one('car.rental.car', string='Car', required=True)
+    name = fields.Char(string='Customer Name', required=True)
+    rental_date = fields.Date(string='Rental Date', required=True)
